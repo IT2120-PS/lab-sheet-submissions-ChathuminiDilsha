e6e608dca37dbfadc729b37f005a8d5e50eec8d1
IT24104107 <chathuminidilsha6@gmail.com>
2025-10-16 19:51:30 +0530

    Delete Lab Sheet 10/IT24104107/IT24104107.R

--- Lab Sheet 10/IT24104107/IT24104107.R
+++
@@ -1,8 +0,0 @@
-setwd ("C:\\Users\\chath\\Desktop\\IT24104107")
-
-# Store the observed purchase counts in a vector
-observed_counts <- c(120, 95, 85, 100)
-
-# Perform the chi-squared test
-# The function assumes equal probabilities by default
-chisq.test(observed_counts)
